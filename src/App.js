83e70a955db98d4b0c7178badc80f0063b3fef98
Anderson Marzola <ajmarzola@gmail.com>
2023-06-21 18:36:46 -0300

    finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React, { Component, useState } from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends Component {  
   render() {
+    const [cep, setCep] = useState('');
+    const [rua, setRua] = useState('');
+    const [bairro, setBairro] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [estado, setEstado] = useState('');
+    const [endereco, setEndereco] = useState({});
+    const [enderecos, setEnderecos] = useState([]);
 
-  const [nome, setNome] = useState('A.J.');
+    function manipularEndereco(evento) {
+      const entrada = evento.target.value;
+      
+      if (entrada.length === 8) {
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+          .then(resposta => resposta.json())
+          .then(dados => {
+            setEndereco({cep: dados.cep, rua: dados.logradouro, bairro: dados.bairro, cidade: dados.localidade, estado: dados.uf});
+            setCep(dados.cep);
+            setRua(dados.logradouro);
+            setBairro(dados.bairro);
+            setCidade(dados.localidade);
+            setEstado(dados.uf);
+            setEnderecos([...enderecos, endereco]);
+          });
 
-  function alterarNome() {
-    setNome('Anderson Marzola');
-  }
+      }
+    }  
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1>{nome}</h1>
-          <button onClick={alterarNome}>Exibir Nome Completo</button>
+          <input placeholder='Digite o CEP' onChange={manipularEndereco} />
+          <ul>
+            <li>CEP: {cep}</li>
+            <li>Rua: {rua}</li>
+            <li>Bairro: {bairro}</li>
+            <li>Cidade: {cidade}</li>
+            <li>Estado: {estado}</li>
+          </ul>
         </header>
       </div>
     );
