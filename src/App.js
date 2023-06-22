46cbbae98e5d8f32866703fcd42e7dd22440a67d
Anderson José Marzola <ajmarzola@gmail.com>
2023-06-22 00:00:55 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
 import React, { Component, useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {  
-  render() {
-    const [cep, setCep] = useState('');
-    const [rua, setRua] = useState('');
-    const [bairro, setBairro] = useState('');
-    const [cidade, setCidade] = useState('');
-    const [estado, setEstado] = useState('');
-    const [endereco, setEndereco] = useState({});
-    const [enderecos, setEnderecos] = useState([]);
+function App() {
+  const [cep, setCep] = useState('');
+  const [rua, setRua] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [estado, setEstado] = useState('');
+  const [endereco, setEndereco] = useState({});
+  const [enderecos, setEnderecos] = useState([]);
 
-    function manipularEndereco(evento) {
-      const entrada = evento.target.value;
-      
-      if (entrada.length === 8) {
-        fetch(`https://viacep.com.br/ws/${cep}/json/`)
-          .then(resposta => resposta.json())
-          .then(dados => {
-            setEndereco({cep: dados.cep, rua: dados.logradouro, bairro: dados.bairro, cidade: dados.localidade, estado: dados.uf});
-            setCep(dados.cep);
-            setRua(dados.logradouro);
-            setBairro(dados.bairro);
-            setCidade(dados.localidade);
-            setEstado(dados.uf);
-            setEnderecos([...enderecos, endereco]);
-          });
+  function manipularEndereco(evento) {
+    const entrada = evento.target.value;
 
-      }
-    }  
+    if (entrada.length === 8) {
+      fetch(`https://viacep.com.br/ws/${entrada}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco({ cep: dados.cep, rua: dados.logradouro, bairro: dados.bairro, cidade: dados.localidade, estado: dados.uf });
+          setCep(dados.cep);
+          setRua(dados.logradouro);
+          setBairro(dados.bairro);
+          setCidade(dados.localidade);
+          setEstado(dados.uf);
+          setEnderecos([...enderecos, endereco]);
+        });
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <input placeholder='Digite o CEP' onChange={manipularEndereco} />
-          <ul>
-            <li>CEP: {cep}</li>
-            <li>Rua: {rua}</li>
-            <li>Bairro: {bairro}</li>
-            <li>Cidade: {cidade}</li>
-            <li>Estado: {estado}</li>
-          </ul>
-        </header>
-      </div>
-    );
+    }
   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder='Digite o CEP' onChange={manipularEndereco} />
+        <ul>
+          <li>CEP: {cep}</li>
+          <li>Rua: {rua}</li>
+          <li>Bairro: {bairro}</li>
+          <li>Cidade: {cidade}</li>
+          <li>Estado: {estado}</li>
+        </ul>
+      </header>
+    </div>
+  );
 }
 
 export default App;
